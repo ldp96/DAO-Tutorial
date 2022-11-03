1ef281e1eca70238de992db78ef682942875a177
Lorenzo Di Puorto <l_dp96@yahoo.it>
2022-11-03 09:35:47 +0100

    dao

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x3cB3F58e0F7D9D776587c16c991dF25d7E4BF978";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const REACT_APP_ALCHEMY_API_URL = process.env.REACT_APP_ALCHEMY_API_URL;
+const REACT_APP_PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: REACT_APP_ALCHEMY_API_URL,
+      accounts: [REACT_APP_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
+  },
+};
